0d4f7b22ff95c59522725529462dd6b33cad7706
tuhaoping <haoping.tu.0105@gmail.com>
2018-11-17 03:04:56 +0800

    Add config.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask
+import config
+
 app = Flask(__name__)
+app.config.from_object(config)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-@app.route("/test")
-def test():
-    return "Hello, test!"
+@app.route("/court/<sc>")
+def court(sc):
+    return "選擇的場地為{}".format(sc)
 
 if __name__ == "__main__":
     app.run()
