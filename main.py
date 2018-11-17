c39a5205f770c1d7557e40a69bbdb77db8d1919f
tuhaoping <haoping.tu.0105@gmail.com>
2018-11-17 23:07:56 +0800

    basic template

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    sport_center = ['文山', '大安', '大同', '松山', '信義', '內湖', '士林', '萬華', '南港', '中正', '北投', '中山']
+    return render_template('index.html', sport_center=sport_center)
 
 @app.route("/court/<sc>")
 def court(sc):
