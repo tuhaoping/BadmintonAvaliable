ba9eb9bf9c5d2684bfdec8bd6f1b7e04c5650785
tuhaoping <haoping.tu.0105@gmail.com>
2018-11-23 23:27:10 +0800

    Result Page initinal

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
+import requests
+import time
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route("/")
 def index():
-    sport_center = [
-        {'id':'ws', 'name':'文山'}, 
-        {'id':'da', 'name':'大安'}, 
-        # {'id':'dt', 'name':'大同'}, 
-        {'id':'ss', 'name':'松山'}, 
-        {'id':'xy', 'name':'信義'}, 
-        {'id':'nh', 'name':'內湖'}, 
-        {'id':'sl', 'name':'士林'}, 
-        {'id':'wh', 'name':'萬華'}, 
-        {'id':'ng', 'name':'南港'}, 
-        {'id':'jj', 'name':'中正'}, 
-        {'id':'bt', 'name':'北投'}, 
-        {'id':'cs', 'name':'中山'}, 
-    ]
+    sport_center = {
+        'ws':{'name':'文山'}, 
+        'da':{'name':'大安'}, 
+        # 'dt':{'name':'大同'}, 
+        'ss':{'name':'松山'}, 
+        'xy':{'name':'信義'}, 
+        'nh':{'name':'內湖'}, 
+        'sl':{'name':'士林'}, 
+        'wh':{'name':'萬華'}, 
+        'ng':{'name':'南港'}, 
+        'jj':{'name':'中正'}, 
+        'bt':{'name':'北投'}, 
+        'cs':{'name':'中山'}, 
+    }
     return render_template('index.html', sport_center=sport_center)
 
-@app.route("/court/<sc>")
-def court(sc):
-    return "選擇的場地為{}".format(sc)
+@app.route("/court", methods=['post'])
+def court():
+    sc = request.form.getlist('sclist')
+    date = request.form.get('date').replace(' / ', '-')
+    msg = []
+    for sportcenter in sc:
+        url = "http://booking.tpsc.sporetrofit.com/Location/findAllowBookingList?LID={}SC&categoryId=Badminton&useDate={}"
+        url = url.format(sportcenter.upper(), date)
+        payload = {
+            "nd": time.time(),
+            "rows": "100",
+            "page": "1",
+        }
+
+        res = requests.post(url=url, data=payload)
+        # print(res)
+        data = res.json()
+        # print(data)
+        if 'errorMsg' in data:
+            msg.append([1, sportcenter])
+        else:
+            avaliable = list(filter(lambda x:x['allowBooking'] == 'Y', data['rows']))
+            if avaliable:
+                for row in avaliable:
+                    msg.append([row['LIDName'], row['LSIDName'], row['StartTime']['Hours'], row['EndTime']['Hours']])
+            else:
+                msg.append([2, sportcenter])
+        
+    return render_template('court.html', messages=msg, date=date)
 
 if __name__ == "__main__":
     app.run()
